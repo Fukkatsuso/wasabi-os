e9b2daa5bbe59d96ea4eb30ccbe8ce0513830fbb
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2025-07-13 12:35:12 +0900

    ほかのタスクに処理を譲る (yield)

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use core::fmt::Write;
 use core::panic::PanicInfo;
 use core::writeln;
 use wasabi::error;
+use wasabi::executor::yield_execution;
 use wasabi::executor::Executor;
 use wasabi::executor::Task;
 use wasabi::graphics::draw_test_pattern;
@@ -101,13 +102,24 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
     // OS によってページテーブルの原本が書き換えられたので、TLB の中身を消すことで再度反映させる
     flush_tlb();
 
-    let task = Task::new(async {
-        info!("Hello from the async world!");
+    let task1 = Task::new(async {
+        for i in 100..=103 {
+            info!("{i}");
+            yield_execution().await;
+        }
+        Ok(())
+    });
+    let task2 = Task::new(async {
+        for i in 200..=203 {
+            info!("{i}");
+            yield_execution().await;
+        }
         Ok(())
     });
 
     let mut executor = Executor::new();
-    executor.enqueue(task);
+    executor.enqueue(task1);
+    executor.enqueue(task2);
     Executor::run(executor)
 }
 
